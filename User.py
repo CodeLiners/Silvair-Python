486fa78589a12eb83f1b0dcf698548bae4d1a0b3
Stiepen22 <stephan@STEPHANLINUX.(none)>
2013-04-20 20:15:05 +0200

    Trying to replace tabs with spaces

--- User.py
+++ User.py
@@ -9,24 +9,24 @@ class User:
         self.clients = []
         self.connectToServer()
 
-	def sendLineToServer(self, line):
-		self.servcon.sendLine(line)
+    def sendLineToServer(self, line):
+    	self.servcon.sendLine(line)
 
-	def sendToServer(self, *d, **data):
-		self.servcon.send(*d, **data)
+    def sendToServer(self, *d, **data):
+        self.servcon.send(*d, **data)
 
-	def broadcastLineToClients(self, line):
-		for client in self.clients:
-			client.sendLine(line)
+    def broadcastLineToClients(self, line):
+        for client in self.clients:
+            client.sendLine(line)
 
 	def broadcastToClients(self, *d, **data):
-		for client in self.clients:
-			client.send(*d, **data)
+        for client in self.clients:
+            client.send(*d, **data)
 
 	def setNick(self, nick):
-		self.sendToServer(args = ["NICK", nick])
-		
+        self.sendToServer(args = ["NICK", nick])
+
     def connectToServer(self):
-    	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    	sock.connect((self.data['server']['host'], self.data['server']['port']))
-    	self.servcon = ServerCon(sock, self.data['server'], self)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        sock.connect((self.data['server']['host'], self.data['server']['port']))
+        self.servcon = ServerCon(sock, self.data['server'], self)
